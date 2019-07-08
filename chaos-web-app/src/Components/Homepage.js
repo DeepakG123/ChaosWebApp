72b41972e69caab9f484c279416d6a030494f7e8
Deepak Goel <goel.deepak9@gmail.com>
2019-07-08 10:35:03 -0400

    App set up with ant-design

---
+++ chaos-web-app/src/Components/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'antd/dist/antd.css';
+import { PageHeader, Carousel } from 'antd';
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+    <PageHeader title="Surge" subTitle="About our Product" />
+    </div>
+  );
+}
+
+export default Homepage;
