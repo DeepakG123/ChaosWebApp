bebb3646c1ce1b36ac4ccafa10a71c94f4568af1
jk4gz <jk4gz@virginia.edu>
2019-07-09 12:02:42 -0400

    attempted to add firebase, unsure if actually connected

--- chaos-web-app/src/App.js
+++ chaos-web-app/src/App.js
@@ -4,6 +4,20 @@ import 'antd/dist/antd.css';
 import Home from "./Components/Homepage.js"
 import testPage from "./Components/testPage.js"
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import firebase from 'firebase'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBT3sRryVV0APlRS9Hcqb6-LsgcSSSdw4g",
+  authDomain: "caas-a3e3c.firebaseapp.com",
+  databaseURL: "https://caas-a3e3c.firebaseio.com",
+  projectId: "caas-a3e3c",
+  storageBucket: "",
+  messagingSenderId: "170546946739",
+  appId: "1:170546946739:web:71f357a88b071e0b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
